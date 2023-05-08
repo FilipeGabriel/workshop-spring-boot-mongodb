eacdd12112854ff3d53f8b09d6b1c4acd5e5a503
Filipe Gabriel <filipegabriel11@hotmail.com>
2023-05-08 19:33:10 -0300

    First commit

---
+++ src/test/java/com/filipegabriel/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filipegabriel.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
