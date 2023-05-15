e1ec6c2c24c6e44beb72b8b2cc2a6c28a617b7ae
Filipe Gabriel <filipegabriel11@hotmail.com>
2023-05-15 20:01:35 -0300

    User Insertion created

--- src/main/java/com/filipegabriel/workshopmongo/services/UserService.java
+++ src/main/java/com/filipegabriel/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.filipegabriel.workshopmongo.domain.User;
+import com.filipegabriel.workshopmongo.dto.UserDTO;
 import com.filipegabriel.workshopmongo.repository.UserRepository;
 import com.filipegabriel.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = userRepository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 	
 }
