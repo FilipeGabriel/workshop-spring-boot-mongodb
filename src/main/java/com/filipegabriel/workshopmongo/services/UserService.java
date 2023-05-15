a55760a4ec9c748bfd2b5aa008dc9aeeec3ff46e
Filipe Gabriel <filipegabriel11@hotmail.com>
2023-05-15 20:15:09 -0300

    User Deletion created

--- src/main/java/com/filipegabriel/workshopmongo/services/UserService.java
+++ src/main/java/com/filipegabriel/workshopmongo/services/UserService.java
@@ -30,6 +30,11 @@ public class UserService {
 		return userRepository.insert(obj);
 	}
 	
+	public void delete(String id) {
+		User user = findById(id);
+		userRepository.delete(user);;
+	}
+	
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
