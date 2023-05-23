bf9df61b5b61a95c97649058c1d63ed8646a40e8
Filipe Gabriel <filipegabriel11@hotmail.com>
2023-05-23 00:32:30 -0300

    SearchTitle with annotation @Query

--- src/main/java/com/filipegabriel/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/filipegabriel/workshopmongo/repository/PostRepository.java
@@ -3,11 +3,16 @@ package com.filipegabriel.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.filipegabriel.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
 	
+	@Query("{ 'title' : { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+
 	
 }
